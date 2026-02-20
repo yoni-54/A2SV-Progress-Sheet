64ae17e162b95020c746640360228d20fadaf7a2
Yonatan <yonatan.daniel54@gmail.com>
2026-02-20 13:15:29 +0300

    Solved Diagonal Taverse and Rotate Images

---
+++ Diagonal_Traverse.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        if not mat:
+            return []
+        
+        m, n = len(mat), len(mat[0])
+        result = []
+        
+        for d in range(m + n - 1):
+            intermediate = []
+            
+            r = 0 if d < n else d - n + 1
+            c = d if d < n else n - 1
+            
+            while r < m and c >= 0:
+                intermediate.append(mat[r][c])
+                r += 1
+                c -= 1
+            
+            if d % 2 == 0:
+                result.extend(intermediate[::-1])
+            else:
+                result.extend(intermediate)
+        
+        return result
