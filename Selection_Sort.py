d532a2f6fb3fdfe1a06d1aad4339f3027a95647d
Yonatan <yonatan.daniel54@gmail.com>
2026-02-26 21:12:32 +0300

    Solved Selection Sort

---
+++ Selection_Sort.py
@@ -0,0 +1,13 @@
+class Solution: 
+    def selectionSort(self, arr):
+        #code here
+        n = len(arr)
+
+        for i in range(n):
+            min_index = i
+
+            for j in range(i + 1, n):
+                if arr[j] < arr[min_index]:
+                    min_index = j
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+        return arr
